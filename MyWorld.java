4a94f673bd7781edb98fe8fbfc0d99937dbbc6f8
judek21 <101126546+judek21@users.noreply.github.com>
2022-04-29 10:26:17 -0400

    a

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,15 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Bee a = new Bee();
+        addObject(a, 200, 200);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
